435eac43ab341ee1ea419c876adb258bc6facbf0
M. Samsioe <michael.samsioe@gmail.com>
2022-07-15 17:14:17 +0000

    Add function start() that checks for a name or adds one

--- run.py
+++ run.py
@@ -12,5 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('portfolio_tracker')
 
-test = SHEET.worksheet("trades").get_all_values()
-print(test)
+btc_price = SHEET.worksheet("price").get_all_values()
+#print(test)
+
+def start():
+    print("Welcome to your bitcoin portfoliotracker")
+    if SHEET.worksheet("name").get_values() == []:
+        print("Please pick a name for your portfolio")
+        portfolio_name_input = [input("Please enter your portfolio name:")]
+        SHEET.worksheet("name").append_row(portfolio_name_input)
+    else:
+        portfolio_name = SHEET.worksheet("name").get_values()
+        print(...)
+        print(f"your portfolio {str(portfolio_name[0][0])} Is now loaded!")
+
+
+
+start()
