37fb74f07fb29d03a38c22b8d42777fdf95b1461
M. Samsioe <michael.samsioe@gmail.com>
2022-07-19 22:10:52 +0000

    Add comment to trade.py

--- trade.py
+++ trade.py
@@ -1,12 +1,17 @@
 class Trade:
+
     def __init__(self, number, date, trade_type, amount, price):
         self.number = number
         self.date = date
         self.trade_type = trade_type
         self.amount = amount
         self.price = price
-
+        """ code below imitated from youtube video
+        https://www.youtube.com/watch?v=9ciQeqyuiek&t=187s
+        by Eybar Vasquez Nevarez
+        """
     def __str__(self):
+        """Method prints the trade instance created"""
         return (
             "\nTrade number; "
             + self.number
@@ -15,7 +20,7 @@ class Trade:
             + "\nTrade type: "
             + self.trade_type
             + "\nBTC amount: "
-            + self.amount                
+            + self.amount
             + "\nBTC price: "
             + self.price
             )
